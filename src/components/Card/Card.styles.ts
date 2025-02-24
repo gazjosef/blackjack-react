b0e9f232ad72d9ebe3ed54c33bd577bf1a2a2eed
Gareth Hind <garethjhind@gmail.com>
2025-02-24 21:46:16 +1100

    Refactor: Separate Sheets

--- src/components/Card/Card.styles.ts
+++ src/components/Card/Card.styles.ts
@@ -1,16 +1,75 @@
 import styled from "styled-components";
+import { suitImages } from "../Card/Card.images";
 
-export const CardContainer = styled.div`
-  width: 60px;
-  height: 90px;
+// Styled Card Component
+export const CardWrapper = styled.div<{ suit: string }>`
+  --width: 5em;
+  --height: calc(var(--width) * 1.7);
+  width: var(--width);
+  height: var(--height);
   background-color: white;
-  border-radius: 8px;
-  border: 1px solid #ddd;
+  border: 1px solid #000;
+  border-radius: 0.25em;
+  padding: 1em;
+
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(7, 1fr);
+  position: relative;
+
+  color: ${(props) =>
+    props.suit === "heart" || props.suit === "diamond" ? "red" : "black"};
+`;
+
+// Styled Pip Component (for non-face cards)
+export const Pip = styled.div<{
+  suit: string;
+  row: number;
+  col: number;
+  rotate?: boolean;
+}>`
+  width: 100%;
+  aspect-ratio: 1 / 1;
+  background-image: url(${(props) => suitImages[props.suit]});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  grid-row-start: ${(props) => props.row};
+  grid-column-start: ${(props) => props.col};
+  transform: ${(props) => (props.rotate ? "rotate(180deg)" : "none")};
+`;
+
+// Styled Corner Numbers
+export const CornerNumber = styled.div<{
+  position: "top" | "bottom";
+  suit: string;
+}>`
+  position: absolute;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  color: black;
-  font-weight: bold;
-  box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.3);
+  font-size: 0.8em;
+  letter-spacing: -0.1em;
+  ${(props) =>
+    props.position === "top"
+      ? "top: 0.25em; left: 0.25em;"
+      : "bottom: 0.25em; right: 0.25em; transform: rotate(180deg);"}
+  &::after {
+    line-height: 0;
+    display: block;
+    width: 0.5em;
+    content: url(${(props) => suitImages[props.suit]});
+  }
+`;
+
+// Styled Face Image (for J, Q, K)
+export const FaceImage = styled.img`
+  grid-row: 2 / span 5;
+  grid-column: 1 / -1;
+  width: 80%;
+  margin: auto;
+  object-fit: contain;
+  pointer-events: none;
+  user-select: none;
 `;
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,25 +1,22 @@
-// Card.tsx
 import React from "react";
-import styled from "styled-components";
-import { suitImages, faceImages } from "../Card/Card.images";
+import { faceImages } from "../Card/Card.images";
+import { CardWrapper, Pip, CornerNumber, FaceImage } from "../Card/Card.styles";
 
-// // Define suit images for pips
-// const suitImages: Record<string, string> = {
-//   heart: "/images/heart.svg",
-//   diamond: "/images/diamond.svg",
-//   club: "/images/club.svg",
-//   spade: "/images/spade.svg",
-// };
-
-// // Define face images mapping for J, Q, K using colour
-// const faceImages: Record<string, string> = {
-//   "J-red": "/images/jack-red.svg",
-//   "Q-red": "/images/queen-red.svg",
-//   "K-red": "/images/king-red.svg",
-//   "J-black": "/images/jack-black.svg",
-//   "Q-black": "/images/queen-black.svg",
-//   "K-black": "/images/king-black.svg",
-// };
+type Suit = "heart" | "diamond" | "club" | "spade";
+type Value =
+  | "A"
+  | "2"
+  | "3"
+  | "4"
+  | "5"
+  | "6"
+  | "7"
+  | "8"
+  | "9"
+  | "10"
+  | "J"
+  | "Q"
+  | "K";
 
 // Define the pip placements for non-face cards
 const pipPositions: Record<
@@ -99,99 +96,11 @@ const pipPositions: Record<
     { row: 7, col: 1, rotate: true },
     { row: 7, col: 3, rotate: true },
   ],
-  // For face cards we won't use pips.
   J: [],
   Q: [],
   K: [],
 };
 
-// Styled Card Component
-const CardWrapper = styled.div<{ suit: string }>`
-  --width: 5em;
-  --height: calc(var(--width) * 1.7);
-  width: var(--width);
-  height: var(--height);
-  background-color: white;
-  border: 1px solid #000;
-  border-radius: 0.25em;
-  padding: 1em;
-
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: repeat(7, 1fr);
-  position: relative;
-
-  color: ${(props) =>
-    props.suit === "heart" || props.suit === "diamond" ? "red" : "black"};
-`;
-
-// Styled Pip Component (for non-face cards)
-const Pip = styled.div<{
-  suit: string;
-  row: number;
-  col: number;
-  rotate?: boolean;
-}>`
-  width: 100%;
-  aspect-ratio: 1 / 1;
-  background-image: url(${(props) => suitImages[props.suit]});
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: cover;
-  grid-row-start: ${(props) => props.row};
-  grid-column-start: ${(props) => props.col};
-  transform: ${(props) => (props.rotate ? "rotate(180deg)" : "none")};
-`;
-
-// Styled Corner Numbers
-const CornerNumber = styled.div<{ position: "top" | "bottom"; suit: string }>`
-  position: absolute;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  font-size: 0.8em;
-  letter-spacing: -0.1em;
-  ${(props) =>
-    props.position === "top"
-      ? "top: 0.25em; left: 0.25em;"
-      : "bottom: 0.25em; right: 0.25em; transform: rotate(180deg);"}
-  &::after {
-    line-height: 0;
-    display: block;
-    width: 0.5em;
-    content: url(${(props) => suitImages[props.suit]});
-  }
-`;
-
-// Styled Face Image (for J, Q, K)
-const FaceImage = styled.img`
-  grid-row: 2 / span 5;
-  grid-column: 1 / -1;
-  width: 80%;
-  margin: auto;
-  object-fit: contain;
-  pointer-events: none;
-  user-select: none;
-`;
-
-type Suit = "heart" | "diamond" | "club" | "spade";
-type Value =
-  | "A"
-  | "2"
-  | "3"
-  | "4"
-  | "5"
-  | "6"
-  | "7"
-  | "8"
-  | "9"
-  | "10"
-  | "J"
-  | "Q"
-  | "K";
-
-// Card Component
 const Card: React.FC<{
   suit: Suit;
   value: Value;
@@ -207,7 +116,6 @@ const Card: React.FC<{
 
   return (
     <CardWrapper suit={suit} data-suit={suit} data-value={value}>
-      {/* Corner Numbers */}
       <CornerNumber suit={suit} position="top">
         {value}
       </CornerNumber>
